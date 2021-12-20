b38bae02af40ec36f5a67c553250be154b4974c4
shadowinhaze <shadowinhaze@gmail.com>
2021-12-20 04:00:54 +0100

    feat: add values filters

--- christmas/src/app__cmas-tree/components/filter/filter.ts
+++ christmas/src/app__cmas-tree/components/filter/filter.ts
@@ -17,14 +17,23 @@ export class Filter extends Component {
 
   private defaultData: DataItems;
 
-  public static extremaDots = <{ [prop: string]: { min: number; max: number } }>{
+  private static extremaDots = <{ [prop: string]: { min: number; max: number } }>{
     count: { min: 0, max: 0 },
     year: { min: 0, max: 0 },
   };
 
-  public static filtersConfig = <{ [prop: string]: Array<number> }>{
+  private static valuesCollection = <{ [prop: string]: Array<string> }>{
+    shape: [],
+    color: [],
+    size: [],
+  };
+
+  private static filtersConfig = <{ [prop: string]: Array<number | string> }>{
     count: [],
     year: [],
+    shape: [],
+    color: [],
+    size: [],
   };
 
   constructor(data: DataItems) {
@@ -32,16 +41,25 @@ export class Filter extends Component {
     this.container?.classList.add(Filter.ClassNames.containerClassName);
     this.data = data;
     this.defaultData = data;
-    this.countExtremaForDoubleSliders(data);
+    this.grubExtremaForDoubleSliders(data);
+    this.grubValuesForValuesCollection(data);
   }
 
-  private countExtremaForDoubleSliders(arr: DataItems): void {
+  private grubExtremaForDoubleSliders(arr: DataItems): void {
     Object.keys(Filter.extremaDots).forEach((extremum) => {
       const allValues: Set<number> = new Set();
       arr.forEach((item) => allValues.add(+item[extremum]));
       const allValuesSorted = [...allValues].sort((a, b) => (a < b ? -1 : 1));
       Filter.extremaDots[extremum].min = allValuesSorted[0];
       Filter.extremaDots[extremum].max = allValuesSorted[allValuesSorted.length - 1];
+    });
+  }
+
+  private grubValuesForValuesCollection(arr: DataItems): void {
+    Object.keys(Filter.valuesCollection).forEach((collectionName) => {
+      const allValues: Set<string> = new Set();
+      arr.forEach((item) => allValues.add('' + item[collectionName]));
+      Filter.valuesCollection[collectionName] = [...allValues];
     });
   }
 
@@ -70,12 +88,41 @@ export class Filter extends Component {
     });
   }
 
+  private addValueFiltersListener(parent: HTMLElement): void {
+    parent.addEventListener('click', (e) => {
+      const clickTarget = <HTMLElement>e.target;
+      const isProper = clickTarget.classList.contains('value-filter__item');
+      const isActive = clickTarget.classList.contains('value-filter__item_active');
+      if (isProper) {
+        const filterType = clickTarget.dataset.filterType;
+        const filterValue = clickTarget.dataset.filterValue;
+        if (isActive) {
+          clickTarget.classList.toggle('value-filter__item_active');
+          if (filterType && filterValue) {
+            Filter.filtersConfig[filterType] = Filter.filtersConfig[filterType].filter((item) => item !== filterValue);
+          }
+        } else {
+          clickTarget.classList.toggle('value-filter__item_active');
+          if (filterType && filterValue) {
+            Filter.filtersConfig[filterType].push(filterValue);
+          }
+        }
+      }
+      this.dataFilter();
+      this.updateFilteredContent();
+    });
+  }
+
   private dataFilter(): void {
     const filterIt = (arr: DataItems, prop: string): DataItems => {
       if (Filter.filtersConfig[prop].length === 0) return arr;
-      return arr.filter(
-        (item) => +item[prop] >= Filter.filtersConfig[prop][0] && +item[prop] <= Filter.filtersConfig[prop][1]
-      );
+      if (prop === 'count' || prop === 'year') {
+        return arr.filter(
+          (item) => +item[prop] >= Filter.filtersConfig[prop][0] && +item[prop] <= Filter.filtersConfig[prop][1]
+        );
+      } else {
+        return arr.filter((item) => Filter.filtersConfig[prop].some((_item) => _item === item[prop]));
+      }
     };
 
     Object.keys(Filter.filtersConfig).forEach((filterName, index) => {
@@ -108,9 +155,85 @@ export class Filter extends Component {
     });
   }
 
+  private valueTranslator(value: string): string {
+    switch (value) {
+      case 'желтый':
+        return 'yellow';
+      case 'зелёный':
+        return 'green';
+      case 'красный':
+        return 'red';
+      case 'белый':
+        return 'white';
+      case 'синий':
+        return 'darkblue';
+      case 'шар':
+        return 'ball';
+      case 'фигурка':
+        return 'figure';
+      case 'колокольчик':
+        return 'bell';
+      case 'шишка':
+        return 'cone';
+      case 'снежинка':
+        return 'snowflake';
+      case 'большой':
+        return 'xl';
+      case 'средний':
+        return 'm';
+      case 'малый':
+        return 's';
+      case 'shape':
+        return 'Форма';
+      case 'color':
+        return 'Цвет';
+      case 'size':
+        return 'Размер';
+    }
+
+    return '';
+  }
+
+  private addValueFilters(): void {
+    const valueFiltersContainer = <HTMLElement>(
+      this.container?.querySelector('.sidebar-catalog__navigation__value-filters')
+    );
+    Object.keys(Filter.valuesCollection).forEach((collectionName) => {
+      const valueFilter = document.createElement('li');
+      valueFilter.classList.add(
+        `sidebar-catalog__navigation__value-filters__${collectionName}-filter`,
+        `${collectionName}-filter`,
+        'list-filter'
+      );
+      valueFilter.innerHTML = `<h3 class="filter-header ${collectionName}-filter__header">${this.valueTranslator(
+        collectionName
+      )} игрушек</h3>`;
+      const valueFilterList = document.createElement('ul');
+      valueFilterList.classList.add(
+        `sidebar-catalog__navigation__value-filters__${collectionName}-filter__items`,
+        `${collectionName}-filter__items`,
+        'value-filter__items'
+      );
+      Filter.valuesCollection[collectionName].forEach((filterValue) => {
+        const valueFilterItem = document.createElement('li');
+        valueFilterItem.classList.add('value-filter__item', `value-filter__item_${this.valueTranslator(filterValue)}`);
+        valueFilterItem.title = `Фильтром ${this.valueTranslator(collectionName)} выбрать ${filterValue}`;
+        valueFilterItem.dataset.filterType = collectionName;
+        valueFilterItem.dataset.filterValue = filterValue;
+        valueFilterList.appendChild(valueFilterItem);
+      });
+      valueFilter.appendChild(valueFilterList);
+      valueFiltersContainer?.appendChild(valueFilter);
+    });
+    if (valueFiltersContainer) {
+      this.addValueFiltersListener(valueFiltersContainer);
+    }
+  }
+
   getContent(): HTMLElement | null {
     this.parseFromTemplate(html);
     this.addDoubleSliders();
+    this.addValueFilters();
     return this.container;
   }
 }
